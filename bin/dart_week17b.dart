020701af01415b7bd6e6147b20c049bf2b4337a0
patipan-patisampita <patipan-patisampita@gmail.com>
2023-09-08 09:39:46 +0700

    update

---
+++ bin/dart_week17b.dart
@@ -0,0 +1,11 @@
+import 'package:dart_week17b/anonymouse.dart';
+import 'package:dart_week17b/param_and_return.dart';
+
+void main() {
+  protoType();
+}
+
+void protoType() {
+  setAnonymous();
+  setName();
+}
